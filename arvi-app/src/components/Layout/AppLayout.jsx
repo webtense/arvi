90963b1822a3e4dfa070649902c0a995e1344008
webtense <webtense@gmail.com>
2026-04-08 19:29:53 +0200

    Fix: add MobileNav import to AppLayout, update Prisma version in package.json

--- arvi-app/src/components/Layout/AppLayout.jsx
+++ arvi-app/src/components/Layout/AppLayout.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from './Sidebar';
 import { BottomNav } from './BottomNav';
+import { MobileNav } from './MobileNav';
 import './AppLayout.css';
 
 export const AppLayout = () => {
     return (
         <div className="app-container">
             <Sidebar />
+            <MobileNav />
             <main className="main-content">
                 <div className="content-wrapper">
                     <Outlet />
